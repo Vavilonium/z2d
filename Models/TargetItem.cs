43ba9844fdfc251ecc4aef17c428abea4b078b5e
Vladimir Galushka <voffka.galushka@mail.ru>
2026-07-06 18:29:09 +0500

    feat: add target file reader

---
+++ Models/TargetItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace z2d.Models
+{
+    public sealed class TargetItem
+    {
+        public required string Name { get; init; }
+        public required string Target {  get; init; }
+        public required TargetType Type { get; init; }
+    }
+}
---
+++ Models/TargetType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace z2d.Models
+{
+    public enum TargetType
+    {
+        Url,
+        Ping
+    }
+}
